e4f32b33aaa01cc08b0f60dc2072821864551723
Irene Tressa Boby <irenetressa.boby@fmr.com>
2024-11-14 10:15:13 -0500

    index.js file

--- toys-service/index.js
+++ toys-service/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const toysRoutes = require('./routes/toysRoutes');
 
 const app = express();
 const PORT = 3033;
 
 app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    res.send('Toys Service is running');
-});
+app.use('/toys', toysRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
